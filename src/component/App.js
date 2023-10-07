cc5b1b5cac2b0edf3face1d707a3e3086a1a7219
casualprogram <113277994+casualprogram@users.noreply.github.com>
2023-10-07 13:14:00 -0400

    Add files via upload
    TEMPLATE UPDATED.
    import OPENAI & ChainLang for AI purpose.

---
+++ src/component/App.js
@@ -0,0 +1,13 @@
+import '../style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Bun Real</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
